5a18f1a13ba61659fc095315474526992f046a48
Ravi Shah <ravidhruvishshah@gmail.com>
2025-05-03 00:47:57 -0500

    contactor task test

--- Nucleo-Code/Tests/Test_Task_Contactor.c
+++ Nucleo-Code/Tests/Test_Task_Contactor.c
@@ -1,3 +1,35 @@
 #include "FreeRTOS.h" /* Must come first. */
 #include "Tasks.h" 
 #include "stm32xx_hal.h"
+#include "statusLEDs.h"
+
+// stack for tasks
+StaticTask_t contactortask_test_buffer;
+StackType_t contactortask_test_stack[configMINIMAL_STACK_SIZE];
+
+int main() {
+    HAL_Init();
+    SystemClock_Config();
+    Status_Leds_Init();
+
+    xTaskCreateStatic(
+        Task_Contactors,
+        "Contactor_Task",
+        configMINIMAL_STACK_SIZE,
+        NULL,
+        tskIDLE_PRIORITY + TASK_CONTACTORS_PRIO,
+        contactortask_test_stack,
+        &contactortask_test_buffer
+    );
+
+    // Start the scheduler
+    vTaskStartScheduler();
+
+    error_handler();
+    while (1) {
+        // Scheduler should've started by now
+        // Code should never enter this point
+    }
+
+    return 0;
+}
