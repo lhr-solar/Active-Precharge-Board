a3060f2c32c6149379502c97042b74dc253a8854
Ravi Shah <ravidhruvishshah@gmail.com>
2025-05-10 23:44:06 -0500

    ign state

--- Firmware/Tasks/Inc/ContactorTask.h
+++ Firmware/Tasks/Inc/ContactorTask.h
@@ -4,4 +4,10 @@
 #include "common.h"
 #include "stm32l4xx_hal.h"
 
+typedef enum {
+    IGNITION_OFF = 0,
+    IGNITION_ARRAY,
+    IGNITION_MOTOR
+} ignition_state_t;
+
 #endif
--- Firmware/Tasks/Src/ContactorTask.c
+++ Firmware/Tasks/Src/ContactorTask.c
@@ -1,11 +1,16 @@
 #include "Tasks.h"
 #include "Contactors.h"
+#include "ContactorTask.h"
+#include "CANMetaData.h"
 
 // Bitmap of current fault state
 uint32_t fault_bitmap = 0;
 
+// Current ignition state
+ignition_state_t ign_state = IGNITION_OFF;
+
 /**
- * @brief   Gets current state of BPS over CAN
+ * @brief   Gets current BPS fault state over CAN
  * @return  True/false corresponding to BPS fault/no fault
  */
 static bool getBPSFault() {
@@ -15,11 +20,39 @@ static bool getBPSFault() {
 }
 
 /**
- * @brief   Gets current state of Controls over CAN
+ * @brief   Gets current Controls fault state over CAN
  * @return  True/false corresponding to Controls fault/no fault
  */
 static bool getControlsFault() {
   return true;
+}
+
+/**
+ * @brief   Gets current ignition state over CAN
+ * @return  Possible ignition states from controls: OFF, ARRAY, MOTOR
+ */
+static ignition_state_t getIgnitionState() {
+  // Receive controls IO_STATE
+  CAN_RxHeaderTypeDef rx_header = { 0 };
+  uint8_t rx_data[8] = { 0 };
+  can_status_t status = CAN_EMPTY;
+
+  status = can_recv(hcan1, IO_STATE, &rx_header, rx_data, 0);
+  if (status == CAN_RECV) {
+    if (rx_data[2] & IGNITION_MOTOR) {
+      ign_state = IGNITION_MOTOR;
+    } else if (rx_data[2] & IGNITION_ARRAY) {
+      ign_state = IGNITION_ARRAY;
+    } else {
+      ign_state = IGNITION_OFF;
+    }
+  }
+  
+  else if (status == CAN_ERR) {
+    // TODO: handle CAN error
+  }
+
+  return ign_state;
 }
 
 void Task_Contactor() {
