8111c64c31faba68fb2ef0664b011ff884d60483
Ravi Shah <ravidhruvishshah@gmail.com>
2025-05-15 22:29:07 -0500

    sense fault handler

--- Firmware/Apps/Src/common.c
+++ Firmware/Apps/Src/common.c
@@ -70,7 +70,6 @@ void fault_handler(void) {
 
   // Setup fault CAN message
   CAN_TxHeaderTypeDef tx_header = { 0 };
-  tx_header.StdId = PRECHARGE_TIMEOUT_;
   tx_header.RTR = CAN_RTR_DATA;
   tx_header.IDE = CAN_ID_STD;
   tx_header.DLC = 2;
@@ -82,15 +81,32 @@ void fault_handler(void) {
     // Turn on fault status LED
     Status_Leds_Write(MOTOR_TIMEOUT_FAULT_LED, true);
     // Send fault CAN message
+    tx_header.StdId = PRECHARGE_TIMEOUT_;
     tx_data[0] = (1 << 0); // Motor Precharge Timeout
-  } else if (fault_bitmap & FAULT_ARRAY_PRECHARGE_TIMEOUT) {
+  }
+  else if (fault_bitmap & FAULT_ARRAY_PRECHARGE_TIMEOUT) {
     // Turn on fault status LED
     Status_Leds_Write(ARRAY_TIMEOUT_FAULT_LED, true);
     // Send fault CAN message
+    tx_header.StdId = PRECHARGE_TIMEOUT_;
     tx_data[0] = (1 << 1); // Array Precharge Timeout
   }
-
-  // TODO: add fault LEDs for all other faults...
+  else if (fault_bitmap & FAULT_MOTOR_PRECHARGE_SENSE) {
+    // Turn on fault status LED
+    Status_Leds_Write(MOTOR_SENSE_FAULT_LED, true);
+    // Send fault CAN message
+    tx_header.StdId = CONTACTOR_SENSE;
+    tx_data[0] = (1 << 5); // Motor Precharge Sense Fault
+    // TODO: add other info to all sense fault messages (expected vs. actual sense value)
+  }
+  else if (fault_bitmap & FAULT_ARRAY_PRECHARGE_SENSE) {
+    // Turn on fault status LED
+    Status_Leds_Write(ARRAY_SENSE_FAULT_LED, true);
+    // Send fault CAN message
+    tx_header.StdId = CONTACTOR_SENSE;
+    tx_data[0] = (1 << 8); // Array Precharge Sense Fault
+    // TODO: add other info to all sense fault messages (expected vs. actual sense value)
+  }
 
   if (can_send(hcan1, &tx_header, tx_data, portMAX_DELAY) != CAN_SENT) error_handler();
 
