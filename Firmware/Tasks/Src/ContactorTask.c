f557803db5f445778b6e6a138fe0884714e083f2
Lakshay983 <lakshay45gupta@gmail.com>
2025-05-28 22:03:23 -0500

    pp

--- Firmware/Tasks/Src/ContactorTask.c
+++ Firmware/Tasks/Src/ContactorTask.c
@@ -180,85 +180,34 @@ static void logic_handler() {
 
 }
 
-static void send_contactor_sense() {
-  // Contactor sense payload
-  // CAN_TxHeaderTypeDef tx_header = { 0 };
-  // tx_header.RTR = CAN_RTR_DATA;
-  // tx_header.IDE = CAN_ID_STD;
-  // tx_header.DLC = 2;
-  // tx_header.TransmitGlobalTime = DISABLE;
-  // tx_header.StdId = CONTACTOR_SENSE;
-  // uint8_t tx_data[8] = { 0 };
-
-  // uint8_t array_precharge_fault    = (fault_bitmap & FAULT_ARRAY_PRECHARGE_SENSE) ? 1 : 0;
-  // uint8_t array_precharge_expected = Contactors_Get(ARRAY_PRECHARGE_CONTACTOR);
-  // uint8_t array_precharge_actual   = Contactors_Get(ARRAY_PRECHARGE_CONTACTOR);
-
-  // uint8_t motor_precharge_fault    = (fault_bitmap & FAULT_MOTOR_PRECHARGE_SENSE) ? 1 : 0;
-  // uint8_t motor_precharge_expected = Contactors_Get(MOTOR_PRECHARGE_CONTACTOR);
-  // uint8_t motor_precharge_actual   = Contactors_Get(MOTOR_PRECHARGE_CONTACTOR);
-
-  // uint8_t motor_fault              = 0;
-  // uint8_t motor_expected           = Contactors_Get(MOTOR_CONTACTOR);
-  // uint8_t motor_actual             = Contactors_Get(MOTOR_CONTACTOR);
-
-  // // Pack bits into two bytes
-  // // [8]: array_precharge_fault
-  // // [7]: array_precharge_expected
-  // // [6]: array_precharge_actual
-  // // [5]: motor_precharge_fault
-  // // [4]: motor_precharge_expected
-  // // [3]: motor_precharge_actual
-  // // [2]: motor_fault
-  // // [1]: motor_expected
-  // // [0]: motor_actual
-
-  // uint16_t packed = 0;
-  // packed |= ((array_precharge_fault    & 0x1) << 8);
-  // packed |= ((array_precharge_expected & 0x1) << 7);
-  // packed |= ((array_precharge_actual   & 0x1) << 6);
-  // packed |= ((motor_precharge_fault    & 0x1) << 5);
-  // packed |= ((motor_precharge_expected & 0x1) << 4);
-  // packed |= ((motor_precharge_actual   & 0x1) << 3);
-  // packed |= ((motor_fault              & 0x1) << 2);
-  // packed |= ((motor_expected           & 0x1) << 1);
-  // packed |= ((motor_actual             & 0x1) << 0);
-
-  // tx_data[0] = packed & 0xFF;        // Lower 8 bits
-  // tx_data[1] = (packed >> 8) & 0x01; // Only bit 8 is used in the second byte
-
-  // // can_send(hcan1, &tx_header, tx_data, portMAX_DELAY);
-  // CAN_TxHeaderTypeDef tx_header = { 0 };
-  // tx_header.StdId = CONTACTOR_SENSE;
-  // tx_header.RTR = CAN_RTR_DATA;
-  // tx_header.IDE = CAN_ID_STD;
-  // tx_header.DLC = 1;
-  // tx_header.TransmitGlobalTime = DISABLE;
-
-  // uint8_t tx_data[8] = { 0 };
-  // tx_data[0] |= 0x01; // Motor contactor sense is on
-  // tx_data[0] |= 0x08; // Motor Precharge contactor sense is on
-  // if (can_send(hcan1, &tx_header, tx_data, portMAX_DELAY) != CAN_SENT) error_handler();
-}
-
-void Task_Contactor() {
-  // Initialize contactors driver
-  // This MUST happen here....or else
-  Contactors_Init();
-
-  // // DELETE THIS LATER - SEND HELLOWORLD CAN MSG
-  // create payload to send
+static void send_contactor_sense(bool motor_fault, bool motor_precharge_fault, bool array_precharge_fault) {
   CAN_TxHeaderTypeDef tx_header = { 0 };
-  tx_header.StdId = 0x650;
+  tx_header.StdId = 0x400;
   tx_header.RTR = CAN_RTR_DATA;
   tx_header.IDE = CAN_ID_STD;
   tx_header.DLC = 1;
   tx_header.TransmitGlobalTime = DISABLE;
 
   uint8_t tx_data[8] = { 0 };
+  tx_data[0] |= ((Contactors_Get(MOTOR_CONTACTOR) ? 1 : 0) << 0);  // motor contactor actual value
+  tx_data[0] |= ((Contactors_Get(MOTOR_CONTACTOR) ? 1 : 0) << 1);  // motor contactor expected value
+  tx_data[0] |= ((motor_fault ? 1 : 0) << 2);  // motor contactor sense value
 
-  tx_data[0] = 0x01;
-  if (can_send(hcan1, &tx_header, tx_data, portMAX_DELAY) != CAN_SENT) error_handler();
+  tx_data[0] |= ((Contactors_Get(MOTOR_PRECHARGE_CONTACTOR) ? 1 : 0) << 3);  // motor contactor actual value
+  tx_data[0] |= ((Contactors_Get(MOTOR_PRECHARGE_CONTACTOR) ? 1 : 0) << 4);  // motor precharge contactor expected value
+  tx_data[0] |= ((motor_precharge_fault ? 1 : 0) << 5);  // motor precharge contactor sense value
+
+  tx_data[0] |= ((Contactors_Get(ARRAY_PRECHARGE_CONTACTOR) ? 1 : 0) << 6);  // array contactor actual value
+  tx_data[0] |= ((Contactors_Get(ARRAY_PRECHARGE_CONTACTOR) ? 1 : 0) << 7);  // array precharge contactor expected value
+  tx_data[1] |= ((array_precharge_fault ? 1 : 0) << 0);  // array precharge contactor sense value
+
+  can_send(hcan1, &tx_header, tx_data, portMAX_DELAY);
+}
+
+void Task_Contactor() {
+  // Initialize contactors driver
+  // This MUST happen here....or else
+  Contactors_Init();
 
   // Reset status variables/bitmaps
   fault_bitmap = FAULT_NONE;
@@ -294,7 +243,7 @@ void Task_Contactor() {
 
     // }
     logic_handler();
-    send_contactor_sense();
+    send_contactor_sense(false, false, false);
     vTaskDelay(pdMS_TO_TICKS(100)); // Run every 100ms
   }
 }
